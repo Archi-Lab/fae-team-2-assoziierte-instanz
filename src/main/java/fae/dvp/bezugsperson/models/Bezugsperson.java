2656102f38e4177ab024ca790f054397eb305bee
Lukas <a@b.c>
2018-11-01 23:29:06 +0100

    Created a Test for Bezugsperson

--- src/main/java/fae/dvp/bezugsperson/models/Bezugsperson.java
+++ src/main/java/fae/dvp/bezugsperson/models/Bezugsperson.java
@@ -55,6 +55,7 @@ public class Bezugsperson {
                 "Username: " + username + '\'' +
                 "Passwort: " + passwort + '\'' +
                 "Rolle: " + rolle + '}';
+       
     }
 }
 
